33c32f21138b9cab6319540106fb3348dfe5201b
Hasanli Hasan <hasan@hasanli.info>
2023-01-25 16:19:21 +0400

    Auth : Login and Register added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const userRoutes = express.Router();
+const { login, register } = require("../services/auth");
 
 userRoutes.get("/", function (req, res) {
   res.send("Hello World");
 });
 
-userRoutes.post("/auth", (req, res) => {});
+userRoutes.post("/auth", (req, res, next) => {
+  login(req, res, next);
+});
+
+userRoutes.post("/auth/register", (req, res, next) => {
+  register(req, res, next);
+});
 
 module.exports = userRoutes;
--- server/services/auth/index.js
+++ server/services/auth/index.js
@@ -1,6 +1,51 @@
-const { connection } = require("./db");
+const { connection } = require("../../db");
 const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const { trycatch } = require("../../utils/trycatch");
 
-const login = async () => {};
+//Login current user
+const login = trycatch(async (req, res, next) => {
+  const { username, password } = req.body;
+  const [rows] = await (
+    await connection
+  ).execute(`SELECT * FROM users WHERE username = ?`, [username]);
+
+  //Check user
+  const user = rows[0];
+  console.log(user);
+  if (!user) throw new Error("User not found");
+
+  //Check password
+  const passwordIsValid = bcrypt.compareSync(password, user.password);
+  if (!passwordIsValid) throw new Error("Incorrect password.");
+
+  // Generate an access token
+  const accessToken = jwt.sign(
+    { username: user.username },
+    process.env.JWT_SECRET_TOKEN
+  );
+
+  res.json({ accessToken });
+});
+
+//Register new User
+const register = trycatch(async (req, res) => {
+  const { username, password } = req.body;
+  const hashPass = bcrypt.hashSync(password, 10);
+  await (
+    await connection
+  )
+    .query("INSERT INTO users (username, password) VALUES  (?,?)", [
+      username,
+      hashPass,
+    ])
+    .then(([data]) => {
+      return res.status(200).json(data.insertId);
+    })
+    .catch((err) => {
+      throw new Error(err);
+    });
+});
+
+module.exports = { login, register };
