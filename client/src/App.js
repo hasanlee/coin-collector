e5580281e23b1a87bebceee954b2f277bc872262
Hasanli Hasan <hasan@hasanli.info>
2023-01-26 20:23:23 +0400

    init folder structures redux configured tailwind css configured

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className='h-full bg-slate-100 dark:bg-slate-900 duration-300'>
+      <Routes>{/* <Route path='/' exact element={<MainPage />} /> */}</Routes>
+    </div>
+  );
+}
+
+export default App;
